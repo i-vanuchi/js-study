b700650ad237db9219d6683ac477675f0eb71b4b
Israel Vanuchi <israel.vanuchi@outlook.com>
2020-12-07 10:22:19 -0300

    Primeira atividade no curso de JS.

---
+++ Fundamentals-part-I/assignment-one.js
@@ -0,0 +1,16 @@
+/* 
+
+LECTURE: Values and Variables
+
+Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions).
+Log their values to the console.
+
+*/
+
+let country = 'Brazil';
+let continent = 'South America';
+let population = 212.4;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ Fundamentals-part-I/script.js
@@ -0,0 +1,6 @@
+let js = 'amazing';
+if (js === 'amazing') alert("First popup");
+js = 'incredible';
+if (js === 'amazing') alert("Second popup");
+
+console.log(40 * 3);
