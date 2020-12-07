d5b7e6c46fef4309f87d1890f3b77268d1ee065f
Israel Vanuchi <israel.vanuchi@outlook.com>
2020-12-07 14:34:36 -0300

    Segunda atividade do curso de JS.

---
+++ Fundamentals-part-I/assignments.js
@@ -0,0 +1,11 @@
+let country = 'Brazil';
+let continent = 'South America';
+let population = 212.4;
+let isIsland = false;
+let language;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
