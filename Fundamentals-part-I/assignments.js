be90cb98e7a013309efb05224133e0335989708a
Israel Vanuchi <israel.vanuchi@outlook.com>
2020-12-09 10:31:24 -0300

    Sétima atividade do curso de JS (Fundamentals part 1 - Type Conversion and Coercion.

--- Fundamentals-part-I/assignments.js
+++ Fundamentals-part-I/assignments.js
@@ -31,3 +31,13 @@ if (population > 33) {
 } else {
     console.log(`${country}'s population is ${33 - population} below average.`)
 }
+
+// Lecture: Type Conversions and Coercion
+
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // 617
+console.log('19' - '13' + 17); // 23
+console.log('123' < 57); // false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+console.log('7' + 4 - '2') // 72
+console.log(2 * '2' + 5) // 9
