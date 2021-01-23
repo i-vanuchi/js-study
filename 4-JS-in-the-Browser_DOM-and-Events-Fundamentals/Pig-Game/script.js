6ff900a051679c397368f182c69cfbf95d349776
Israel Vanuchi <israel.vanuchi@outlook.com>
2021-01-23 17:20:16 -0300

    Pig-Game: EventListener no botao de rolar o dado e exibição da pontuação atual (current).

--- 4-JS-in-the-Browser_DOM-and-Events-Fundamentals/Pig-Game/script.js
+++ 4-JS-in-the-Browser_DOM-and-Events-Fundamentals/Pig-Game/script.js
@@ -1,11 +1,40 @@
 'use strict';
 
 // Selecting Elements
-const elScore0 = document.querySelector('#score--0');
-const elScore1 = document.querySelector('#score--1');
-const elDice = document.querySelector('.dice');
+const elmScore0 = document.querySelector('#score--0');
+const elmScore1 = document.querySelector('#score--1');
+const elmCurrent0 = document.getElementById('current--0');
+const elmCurrent1 = document.getElementById('current--1');
+const elmDice = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
 
 // Starting conditions
-elScore0.textContent = 0;
-elScore1.textContent = 0;
-elDice.classList.add('hidden');
+elmScore0.textContent = 0;
+elmScore1.textContent = 0;
+elmDice.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling the dice
+btnRoll.addEventListener('click', function() {
+    // Generate the dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // Display the dice img according to the dice number
+    elmDice.src = `dice-${dice}.png`;
+    elmDice.classList.remove('hidden');
+
+    // Check for rolled 1.
+    if(dice !== 1) {
+        // Add dice to current score
+        currentScore += dice;
+        elmCurrent0.textContent = currentScore;
+    } else {
+        // Switch to the other player
+    }
+
+})
