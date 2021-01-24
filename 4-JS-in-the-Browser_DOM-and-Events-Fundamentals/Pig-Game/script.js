32cfa5d4169aca59dac3fe243d48766f162213bb
Israel Vanuchi <israel.vanuchi@outlook.com>
2021-01-24 11:56:57 -0300

    Pig-Game: Alternação de player ativo e currentScore dinâmico

--- 4-JS-in-the-Browser_DOM-and-Events-Fundamentals/Pig-Game/script.js
+++ 4-JS-in-the-Browser_DOM-and-Events-Fundamentals/Pig-Game/script.js
@@ -3,6 +3,8 @@
 // Selecting Elements
 const elmScore0 = document.querySelector('#score--0');
 const elmScore1 = document.querySelector('#score--1');
+const elmPlayer0 = document.querySelector('.player--0');
+const elmPlayer1 = document.querySelector('.player--1');
 const elmCurrent0 = document.getElementById('current--0');
 const elmCurrent1 = document.getElementById('current--1');
 const elmDice = document.querySelector('.dice');
@@ -10,12 +12,19 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+// FUNCTIONS
+// const switchPlayer = function(previousPlayer, nextPlayer) {
+//     previousPlayer.classList.remove('player--active');
+//     nextPlayer.classList.add('player--active');
+// }
 
 // Starting conditions
 elmScore0.textContent = 0;
 elmScore1.textContent = 0;
 elmDice.classList.add('hidden');
 
+
+let activePlayer = 0;
 let currentScore = 0;
 
 // Rolling the dice
@@ -32,9 +41,14 @@ btnRoll.addEventListener('click', function() {
     if(dice !== 1) {
         // Add dice to current score
         currentScore += dice;
-        elmCurrent0.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        
     } else {
-        // Switch to the other player
+        currentScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        elmPlayer0.classList.toggle('player--active');
+        elmPlayer1.classList.toggle('player--active');
     }
 
 })
