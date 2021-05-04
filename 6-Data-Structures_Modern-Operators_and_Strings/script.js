ba90865b3a24be40666976f8050dae809f8fafd3
Israel Vanuchi <israel.vanuchi@outlook.com>
2021-05-04 17:00:37 -0300

    Encadeamento opcional (?.)

--- 6-Data-Structures_Modern-Operators_and_Strings/script.js
+++ 6-Data-Structures_Modern-Operators_and_Strings/script.js
@@ -22,7 +22,7 @@ const restaurant = {
     },
   },
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -340,36 +340,71 @@ const restaurant = {
 
 // ----- Lecture: Enhanced Object Literals -----
 
-const hours = {
-  thu: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  // 3 - Calcular nomes de propriedades
-  [`day ${4 + 2}`]: {
-    open: 0,
-    close: 24,
-  },
-};
+// const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const hours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   fri: {
+//     open: 11,
+//     close: 23,
+//   },
+//   // 3 - Calcular nomes de propriedades
+//   [`day ${4 + 2}`]: {
+//     open: 0,
+//     close: 24,
+//   },
+// };
 
-const anotherRestaurant = {
-  rName: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  // 1 - Criar propriedade a partir de objeto existente
-  hours,
+// const anotherRestaurant = {
+//   rName: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//   // 1 - Criar propriedade a partir de objeto existente
+//   hours,
 
-  // 2 - Escrever métodos com sintaxe mais simples e compacta
-  orderPasta(ing1, ing2, ing3) {
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
-};
-console.log(anotherRestaurant);
+//   // 2 - Escrever métodos com sintaxe mais simples e compacta
+//   orderPasta(ing1, ing2, ing3) {
+//     console.log(
+//       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+//     );
+//   },
+// };
+// console.log(anotherRestaurant);
+
+// ----- Lecture: Optional Chaining (?.) -----
+
+// // SEM encadeamento opcional
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
+
+// // COM encadeamento opcional
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
+
+// // Exemplo
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day} we open at ${open}`);
+// }
+
+// // Métodos
+
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderSomething?.(0, 1) ?? 'Method does not exist');
+
+// // Arrays
+
+// const users = [
+//   { name: 'Israel', email: 'example@mail.com' },
+//   { name: 'Someone', email: 'some-email@mail.com' },
+// ];
+
+// console.log(users[0]?.name ?? 'User does not exist');
+// console.log(users[1]?.email ?? 'User does not exist');
+// console.log(users[2]?.name ?? 'User does not exist');
