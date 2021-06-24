9a41a16fd1692acc815fe2f136e2e7184438c50a
i-vanuchi <israel.vanuchi@outlook.com>
2021-06-24 19:14:46 -0300

    Arrays: Coding Challenge 2

--- 8-Arrays/coding-challenges/coding-challenge-2/script.js
+++ 8-Arrays/coding-challenges/coding-challenge-2/script.js
@@ -1,35 +1,32 @@
 /*
-  Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and store the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
-
+  
 
 */
 
-// Test data 1:
+// test data
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
 
-const dataJulia1 = [3, 5, 2, 12, 7];
-const dataKate1 = [4, 1, 15, 8, 3];
-
-// Test data 2:
-
-const dataJulia2 = [9, 16, 6, 8, 3];
-const dataKate2 = [10, 5, 6, 1, 4];
-
-const checkDogs = function (dogsJulia, dogsKate) {
+function calcAverageHumanAge(ages) {
   // 1
-  const dogsJuliaCopy = dogsJulia.slice(1, -2); // poderia ter sido feito com o splice, como mostrado pelo Jonas
-  console.log(dogsJuliaCopy);
+  const humanAges = ages
+    .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
 
-  // 2
-  const totalDogs = dogsJuliaCopy.concat(dogsKate);
-  console.log(totalDogs);
+    // 2 (com função regular)
+    .filter(dogAge => dogAge >= 18);
 
   // 3
-  totalDogs.forEach(function (age, i) {
-    if (age >= 3)
-      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old.`);
-    else console.log(`Dog number ${i + 1} is still a puppy 🐶.`);
-  });
-};
+  // const dogAgesAverage =
+  //   humanAges.reduce(function (acc, dogAge) {
+  //     return acc + dogAge;
+  //   }, 0) / humanAges.length;
+  // 3 - solução alternativa
+  const dogAgesAverage = humanAges.reduce(function (acc, dogAge, i, arr) {
+    return acc + dogAge / arr.length;
+  }, 0);
 
-// 4
-checkDogs(dataJulia1, dataKate1);
+  return dogAgesAverage;
+}
+
+console.log(calcAverageHumanAge(data1));
+console.log(calcAverageHumanAge(data2));
